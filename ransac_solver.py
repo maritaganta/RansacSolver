8677337b9e2f1dc17ae4b24abbb8c0b103446bb5
Marita Georganta <85311982+maritaganta@users.noreply.github.com>
2023-06-11 20:03:25 +0200

    Update ransac_solver.py

--- ransac_solver.py
+++ ransac_solver.py
@@ -5,33 +5,41 @@ from skimage.measure import LineModelND
 class RansacSolver:
 
     def __init__(self):
+        # Initialize curve fitting model. Here is a sample N-dimensional line model. It can be swapped with any other desired fitting model.
         self.model = LineModelND()
 
     def fit(self, data: Data, threshold=25, iter_num=1500, sample_num=2):
-
+        
+        # Initialization of parameters to keep track of during execution
         counter = 0
         max_inliers = 0
         best = None
 
-        prob_out = 0.5
-        prob_desired = 0.95
-
+        # Starting the iterative process of the algorithm
         while counter < iter_num:
 
           counter = counter + 1
 
+          # Shuffling the data and choosing a random sample for line fitting
           np.random.shuffle(data.data)
           sample = data.data[:sample_num, :]
 
+          # Use random pair to estimate the line
           self.model.estimate(sample)
 
+          # Calculate the y coordinates of all x coordinates of the data according to the fitted line
           y_model = self.model.predict_y(data.x)
+            
+          # Calculate the difference between the y coordinates of the model and the y coordinates of the data
           error = np.abs(data.y - y_model.T)
+            
+          # Count how many points are within the threshold set as distance from the line
           inliers = np.count_nonzero(error < threshold)
 
+          # Iteratively save the model with the best results
           if inliers > max_inliers:
             max_inliers = inliers
             best = self.model.params
 
-
+        # Return the parameters of the model
         return best
